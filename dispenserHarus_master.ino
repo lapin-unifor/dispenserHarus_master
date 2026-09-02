408c665f45583b2f13385989ca7face142e59d05
Daniel Almeida Chagas <daniel.helptech@gmail.com>
2026-09-01 22:41:48 -0300

    Update dispenserHarus_master.ino
    Teste de botão.

--- dispenserHarus_master.ino
+++ dispenserHarus_master.ino
@@ -18,6 +18,16 @@ bool modulo3 = false;
 
 bool estadoLed = false;
 
+//liberado para uso
+bool liberado1 = false;
+bool liberado2 = false;
+bool liberado3 = false;
+
+//status dos reles
+bool rele1 = false;
+bool rele2 = false;
+bool rele3 = false;
+
 //timers
 double timerLeds = 0;
 double timerStatus = 10000;
@@ -34,9 +44,10 @@ void setup() {
   } else {
     mensagem("Módulo 1 ativo");
     modulo1 = true;
-    pcf1.pinMode(0, INPUT_PULLUP);
-    pcf1.pinMode(1, OUTPUT);
-    pcf1.pinMode(2, OUTPUT);
+    pcf1.pinMode(0, INPUT_PULLUP); //botão
+    pcf1.pinMode(1, OUTPUT); //led
+    pcf1.pinMode(2, OUTPUT); //relé
+    pcf1.digitalWrite(2, HIGH);
   }
   if (!pcf2.begin(0x21, &Wire)) {
     mensagem("FALHA! MOD 2 inativo!");
@@ -46,6 +57,7 @@ void setup() {
     pcf2.pinMode(0, INPUT_PULLUP);
     pcf2.pinMode(1, OUTPUT);
     pcf2.pinMode(2, OUTPUT);
+    pcf2.digitalWrite(2, HIGH);
   }
   if (!pcf3.begin(0x22, &Wire)) {
     mensagem("FALHA! MOD 3 inativo!");
@@ -55,6 +67,7 @@ void setup() {
     pcf3.pinMode(0, INPUT_PULLUP);
     pcf3.pinMode(1, OUTPUT);
     pcf3.pinMode(2, OUTPUT);
+    pcf3.digitalWrite(2, HIGH);
   }
 }
 
@@ -75,14 +88,22 @@ void loop() {
     timerStatus = millis() + 10000;
     printStatus();
   }
+
   /*
-  if(pcf.digitalRead(0)==0){
-    estadoRele = !estadoRele;
-    pcf.digitalWrite(2, estadoRele);
-    Serial.print("Rele em ");
-    Serial.println(estadoRele);
-    delay(500);
+  if(pcf1.digitalRead(0)==0){
+    if(liberado1){
+      rele1 = !rele1;
+      mensagem("Acionando rele 1");
+    } else {
+      mensagem("Acesso negado ao Mod1!");
+    }
+    delay(250);
   }
+  
+  
+  if(modulo1) pcf1.digitalWrite(2,rele1);
+  if(modulo2) pcf2.digitalWrite(2,rele2);
+  if(modulo3) pcf3.digitalWrite(2,rele3);
   */
 }
 
