8978a2ee8ab58806184d3e5f0cb5965753e46946
Daniel Almeida Chagas <daniel.helptech@gmail.com>
2026-09-07 10:23:34 -0300

    Incluido LCD

--- dispenserHarus_master.ino
+++ dispenserHarus_master.ino
@@ -5,7 +5,13 @@
 
 
 #include <Adafruit_PCF8574.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x26,20,4);
+String terminal1 = "";
+String terminal2 = "";
+String terminal3 = "";
 
 Adafruit_PCF8574 pcf1;
 Adafruit_PCF8574 pcf2;
@@ -13,6 +19,8 @@ Adafruit_PCF8574 pcf3;
 
 char caractere = ' ';
 String txtRecebido = "";
+
+bool tituloLcd = false;
 
 //módulos
 bool modulo1 = false;
@@ -35,19 +43,28 @@ bool rele3 = false;
 //timers
 double timerLeds = 0;
 double timerLedPiscando = 0;
-double timerStatus = 10000;
+double timerStatus = 30000;
+double timerLcd = 10000;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(19, 20);
+  //Wire.begin(8, 9);
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("  Harus Tecnologia  ");
+  lcd.setCursor(0,1);
+  lcd.print("Iniciando...");
   delay(1000);
   //while (!Serial) { delay(10); }
   mensagem("Dispenser Harus MVP");
 
   if (!pcf1.begin(0x20, &Wire)) {
-    mensagem("FALHA! MOD 1 inativo!");
+    mensagem("FALHA! Disp1 inativo");
   } else {
-    mensagem("Módulo 1 ativo");
+    mensagem("Módulo Disp1 ativo");
     modulo1 = true;
     pcf1.pinMode(0, INPUT_PULLUP); //botão
     pcf1.pinMode(1, OUTPUT); //led
@@ -55,9 +72,9 @@ void setup() {
     pcf1.digitalWrite(2, HIGH);
   }
   if (!pcf2.begin(0x21, &Wire)) {
-    mensagem("FALHA! MOD 2 inativo!");
+    mensagem("FALHA! Disp2 inativo");
   } else {
-    mensagem("Módulo 2 ativo");
+    mensagem("Módulo Disp2 ativo");
     modulo2 = true;
     pcf2.pinMode(0, INPUT_PULLUP);
     pcf2.pinMode(1, OUTPUT);
@@ -65,9 +82,10 @@ void setup() {
     pcf2.digitalWrite(2, HIGH);
   }
   if (!pcf3.begin(0x22, &Wire)) {
-    mensagem("FALHA! MOD 3 inativo!");
+    //        1---5----10---15--20
+    mensagem("FALHA! Disp3 inativo");
   } else {
-    mensagem("Módulo 3 ativo");
+    mensagem("Módulo Disp3 ativo");
     modulo3 = true;
     pcf3.pinMode(0, INPUT_PULLUP);
     pcf3.pinMode(1, OUTPUT);
@@ -82,8 +100,8 @@ void loop() {
     caractere = Serial.read();
     if(caractere=='\n'){
       //executar o comando
-      Serial.print("Voce digitou ");
-      Serial.println(txtRecebido);
+      //Serial.print("Voce digitou ");
+      //Serial.println(txtRecebido);
       maquinaDeEstados(txtRecebido); //chama a maq estados p executar
       txtRecebido = "";
     } else {
@@ -109,40 +127,68 @@ void loop() {
     if(rele2 && modulo2) pcf2.digitalWrite(1,estadoLedPiscando);
     if(rele3 && modulo3) pcf3.digitalWrite(1,estadoLedPiscando);
   }
+
+  //timer de exibir o status do equipamento
   if(timerStatus < millis()){
-    timerStatus = millis() + 10000;
+    timerStatus = millis() + 60000;
     printStatus();
   }
 
-  
-  if(pcf1.digitalRead(0)==0){
-    if(liberado1){
-      rele1 = !rele1;
-      mensagem("Acionando rele 1");
+  //timer de mudar o titulo do LCD
+  if(timerLcd < millis()){
+    timerLcd = millis() + 10000;
+    lcd.setCursor(0,0);
+    lcd.print("                    ");
+    lcd.setCursor(0,0);
+    if(tituloLcd){
+      lcd.print("  Harus Tecnologia  ");
     } else {
-      mensagem("Acesso negado ao Mod1!");
+      //         1---5----10---15--20
+      lcd.print("EcoDispenser MVP 1.0");
     }
-    delay(250);
-  }
-  
-  if(pcf2.digitalRead(0)==0){
-    if(liberado2){
-      rele2 = !rele2;
-      mensagem("Acionando rele 2");
-    } else {
-      mensagem("Acesso negado ao Mod2!");
-    }
-    delay(250);
+    tituloLcd = !tituloLcd;
   }
 
-  if(pcf3.digitalRead(0)==0){
-    if(liberado3){
-      rele3 = !rele3;
-      mensagem("Acionando rele 3");
-    } else {
-      mensagem("Acesso negado ao Mod3!");
+  if(modulo1){
+    if(pcf1.digitalRead(0)==0){
+      if(liberado1){
+        rele1 = !rele1;
+        //        1---5----10---15--20
+        mensagem("Acionando Disp1...");
+      } else {
+        //        1---5----10---15--20
+        mensagem("Acesso negado Disp1!");
+      }
+      delay(250);
     }
-    delay(250);
+  }
+  
+  if(modulo2){
+    if(pcf2.digitalRead(0)==0){
+      if(liberado2){
+        rele2 = !rele2;
+        //        1---5----10---15--20
+        mensagem("Acionando Disp2...");
+      } else {
+        //        1---5----10---15--20
+        mensagem("Acesso negado Disp2!");
+      }
+      delay(250);
+    }
+  }
+
+  if(modulo3){
+    if(pcf3.digitalRead(0)==0){
+      if(liberado3){
+        rele3 = !rele3;
+        //        1---5----10---15--20
+        mensagem("Acionando Disp3...");
+      } else {
+        //        1---5----10---15--20
+        mensagem("Acesso negado Disp3!");
+      }
+      delay(250);
+    }
   }
   
   if(modulo1) pcf1.digitalWrite(2,!rele1);
@@ -154,35 +200,60 @@ void loop() {
 void maquinaDeEstados(String texto){
   //Receber comandos, analisar e chamar as devidas funções
   String comando = texto.substring(0,1); //isto recebe o 1º caractere
-  Serial.println("Comando: " + comando);
+  //Serial.println("Comando: " + comando);
   String parametro = texto.substring(1,texto.length());
-  Serial.println("Parametro: " + parametro);
+  //Serial.println("Parametro: " + parametro);
   //executar os comandos e parâmetros
   switch(comando.charAt(0)){
     case 'l':
-    	mensagem("Liberando dispenser");
+      //        1---5----10---15--20
+    	mensagem("Liberado dispenser " + parametro);
     	if(parametro.toInt() == 1) liberado1 = true;
       if(parametro.toInt() == 2) liberado2 = true;
       if(parametro.toInt() == 3) liberado3 = true;
     	break;
     case 'x':
-    	mensagem("Travando dispenser");
+      //        1---5----10---15--20
+    	mensagem("Travado dispenser " + parametro);
     	if(parametro.toInt() == 1) liberado1 = rele1 = false;
       if(parametro.toInt() == 2) liberado2 = rele2 = false;
       if(parametro.toInt() == 3) liberado3 = rele3 = false;
     	break;
     default:
-    	Serial.println("Comando desconhecido");
+      //        1---5----10---15--20
+    	mensagem("Comando desconhecido");
   }
 }
 
 void printStatus(){
-  mensagem("Status:");
-  if(modulo1) mensagem("Mod1 OK!");
-  if(modulo2) mensagem("Mod2 OK!");
-  if(modulo3) mensagem("Mod3 OK!");
+  String statusTxt = "Status: ";
+  if(modulo1){
+    statusTxt = statusTxt + "[D1]";
+  } else statusTxt = statusTxt + "[--]";
+  if(modulo2) {
+    statusTxt = statusTxt + "[D2]";
+  } else statusTxt = statusTxt + "[--]";
+  if(modulo3) {
+    statusTxt = statusTxt + "[D3]";
+  } else statusTxt = statusTxt + "[--]";
+  mensagem(statusTxt);
 }
 
 void mensagem(String msg){
   Serial.println(msg);
+  lcd.setCursor(0,1);
+  lcd.print("                    ");
+  lcd.setCursor(0,2);
+  lcd.print("                    ");
+  lcd.setCursor(0,3);
+  lcd.print("                    ");
+  terminal1 = terminal2;
+  terminal2 = terminal3;
+  terminal3 = msg;
+  lcd.setCursor(0,1);
+  lcd.print(terminal1.substring(0,20));
+  lcd.setCursor(0,2);
+  lcd.print(terminal2.substring(0,20));
+  lcd.setCursor(0,3);
+  lcd.print(terminal3.substring(0,20));
 }
